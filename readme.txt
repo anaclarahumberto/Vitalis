dd48f90abb765ce8335144502dd2e74b8054cb18
Mila Misa <milamisa77@gmail.com>
2024-07-04 19:28:15 -0300

    commit da versão certa do projeto

---
+++ readme.txt
@@ -0,0 +1,8 @@
+Telas criadas:
+
+Notificações (Camila) => http://127.0.0.1:8000/notificacoes
+Configurações  (Ana Clara) => http://127.0.0.1:8000/configuracoes
+Tempo Gasto (Isabella) => http://127.0.0.1:8000/configuracoes/tempogasto
+Arquivados (Sara) => http://127.0.0.1:8000/configuracoes/arquivados
+
+OBS: O acesso as páginas de tempo gasto e arquivados se encontra dentro da página notificações. 
---
+++ routes/main_routes.py
@@ -0,0 +1,29 @@
+from fastapi import APIRouter, Request
+from fastapi.responses import HTMLResponse
+
+from util.templates import obter_jinja_templates
+
+router = APIRouter()
+templates = obter_jinja_templates("templates/main")
+
+
+@router.get("/", response_class=HTMLResponse)
+async def get_root(request: Request):
+    return templates.TemplateResponse("pages/index.html", {"request": request})
+
+
+@router.get("/notificacoes", response_class=HTMLResponse)
+async def get_notificacoes(request: Request):
+    return templates.TemplateResponse("pages/notificacoes.html", {"request": request}) 
+
+@router.get("/configuracoes", response_class=HTMLResponse)
+async def get_configuracoes(request: Request):
+    return templates.TemplateResponse("pages/configuracoes.html", {"request": request}) 
+
+@router.get("/configuracoes/tempogasto", response_class=HTMLResponse)
+async def get_tempogasto(request: Request):
+    return templates.TemplateResponse("pages/tempogasto.html", {"request": request}) 
+
+@router.get("/configuracoes/arquivados", response_class=HTMLResponse)
+async def get_arquivados(request: Request):
+    return templates.TemplateResponse("pages/arquivados.html", {"request": request}) 
