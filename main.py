dbc9678486870ebda6174ffb8db29bc843dbe898
MilaMisa <milamisa77@gmail.com>
2024-10-18 10:04:56 -0300

    Conserto das páginas de error

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from repositories.usuario_repo import UsuarioRepo
 from routes.main_routes import router as main_router
 from routes.usuario_routes import router as usuario_router
 from util.auth import checar_autenticacao, checar_autorizacao
+from util.exceptions import tratar_excecoes
 
 UsuarioRepo.criar_tabela()
 dotenv.load_dotenv()
@@ -12,5 +13,6 @@ dotenv.load_dotenv()
 app = FastAPI(dependencies=[Depends(checar_autorizacao)])
 app.mount(path="/static", app=StaticFiles(directory="static"), name="static")
 app.middleware("http")(checar_autenticacao)
+tratar_excecoes(app)
 app.include_router(main_router)
 app.include_router(usuario_router)
